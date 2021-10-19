a87ae74c66764c689ae687beac3d900ad8a7de68
suddenAppearance <kuz.damir.01@gmail.com>
2021-10-19 17:10:35 +0300

    Regression Analysis Block 1 completed

---
+++ Block1/task4.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+# Date, Open, High, Low, Close
+with open("HistoricalPrices2.csv") as csv_:
+    # поиск параметров регрессии
+    lines = csv_.readlines()
+    y = list(reversed(list(map(lambda s: float(s.split(", ")[4]), lines))))
+    N = len(y)
+    x = list(reversed(list(map(lambda s: float(s.split(", ")[2]), lines))))
+    x_ = sum(x)
+    y_ = sum(y)
+    xy_ = sum(map(lambda s: s[0] * s[1], [[x[i], y[i]] for i in range(N)]))
+    x2_ = sum([i * i for i in x])
+    b1 = (N * xy_ - x_ * y_) / (N * x2_ - x_ * x_)
+    b0 = (y_ - b1 * x_) / N
+    print(f"Regression function: C = {b0} {'+' if b1 > 0 else '-'} {abs(b1)} * t + e")
+
+    # коэфициент детерминации
+    y_ = y_ / N
+    sse = sum(map(lambda s: (s[0] - s[1]) * (s[0] - s[1]), [[y[i], b0 + b1 * x[i]] for i in range(N)]))
+    sst = sum(map(lambda s: (s[0] - s[1]) * (s[0] - s[1]), [[y[i], y_] for i in range(N)]))
+    r2 = 1 - sse / sst
+    r2_adj = 1 - (1 - r2) * (N - 1) / (N - 2)
+    print(f"R^2 = {r2}")
+    print(f"Adjusted R^2 = {r2_adj}")
+
+    # графики
+
+    plt.plot(x, y, 'ro')
+    plt.plot(x, [b0 + b1 * x[i] for i in range(N)])
+    plt.show()
